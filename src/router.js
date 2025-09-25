258adb2722971e6811f0a3821b1839f261503a4b
Diego <diegollamozas911@gmail.com>
2025-09-25 16:57:45 -0500

    feat(router): add router setup with initial routes and components

---
+++ src/router.js
@@ -0,0 +1,20 @@
+import Home from "./shared/presentation/views/home.vue";
+import {createRouter, createWebHistory} from "vue-router";
+
+const about = () => import('./shared/presentation/views/about.vue');
+const pageNotfound = () => import('./shared/presentation/views/page-not-found.vue');
+
+const routes = [
+    {path: '/home', name: 'home', component: Home, meta: {title: 'Home'}},
+    {path: '/about', name: 'about', component: about, meta: {title: 'About'}},
+    {path: '/', redirect: '/home'},
+    {path: '/:pathMatch(.*)*', name: 'not-found', component: pageNotfound, meta: {title: 'Not Found'}},
+];   
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: routes,
+})
+
+export default router;
+
