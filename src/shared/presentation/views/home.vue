27e6f95277c50f775d039afef934d6d92f991342
Diego <diegollamozas911@gmail.com>
2025-09-26 16:36:12 -0500

    feat(home): integrate internationalization with vue-i18n for home page

--- src/shared/presentation/views/home.vue
+++ src/shared/presentation/views/home.vue
@@ -1,9 +1,14 @@
 <script setup>
+import {useI18n} from "vue-i18n";
 
+const { t } = useI18n()
 </script>
 
 <template>
-
+  <div class="align-content-start justify-content-start m-4">
+    <h1 class="text-3xl font-bold mb-4">{{ t('home.title') }}</h1>
+    <p class="mb-2">{{ t('home.content') }}</p>
+  </div>
 </template>
 
 <style scoped>
