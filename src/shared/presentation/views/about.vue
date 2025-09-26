e2f39121aac055ff59278aa5607017f898276e75
Diego <diegollamozas911@gmail.com>
2025-09-26 16:34:35 -0500

    feat(about): add internationalization support with vue-i18n

--- src/shared/presentation/views/about.vue
+++ src/shared/presentation/views/about.vue
@@ -1,9 +1,14 @@
 <script setup>
+import {useI18n} from "vue-i18n";
 
+const { t } = useI18n()
 </script>
 
 <template>
-
+  <div class="align-content-start justify-content-start m-4">
+    <h1>{{ t('about.title') }}</h1>
+    <image src="../src/assets/vue.svg" alt="ACME Logo" mode="aspectFill" width="200" height="200" />
+  </div>
 </template>
 
 <style scoped>
