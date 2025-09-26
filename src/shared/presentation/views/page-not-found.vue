b2e0a57b83ff6d552534f9236024a947d9af66f6
Diego <diegollamozas911@gmail.com>
2025-09-26 16:40:40 -0500

    feat(page-not-found): add internationalization support with vue-i18n

--- src/shared/presentation/views/page-not-found.vue
+++ src/shared/presentation/views/page-not-found.vue
@@ -1,9 +1,18 @@
 <script setup>
+import {useI18n} from "vue-i18n";
+
+const { t } = useI18n()
+const router = useRouter();
+const unavailableRoute = router.currentRoute.value.fullPath;
 
 </script>
 
 <template>
-
+  <div class="align-content-start justify-content-start m-4">
+    <h1 class="text-3xl font-bold mb-4">{{ t('pageNotFound.title') }}</h1>
+    <p class="mb-2">{{ t('pageNotFound.content', {  'unavailable-router': unavailableRoute }) }}</p>
+    <router-link to="/home">{{ t('page-not-found.go-home') }}</router-link>
+  </div>
 </template>
 
 <style scoped>
